9fecb7a193fe0ae866c53166b44c001515be2207
christerng <christerng@gmail.com>
2023-05-17 20:59:20 +0800

    fix: typo

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use("/queue-scan", queueScanRouter);
 app.use("/wait-times", waitTimesRouter);
-app.ues("volumes", volumesRouter);
+app.use("volumes", volumesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
