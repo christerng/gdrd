eb7c412685a5556a0e62c2db4dce96d3b2df3d1c
Christopher Terence Ng <christerng@gmail.com>
2023-05-20 19:53:40 +0800

    feat: add routes and set default landing page to `/api/volumes`

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
-const express = require('express');
-const path = require('path');
+var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
+var debug = require('debug')('backend:server');
+var http = require('http');
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
 
-const app = express();
+var indexRouter = require('./api/index');
+var lengthsRouter = require('./api/lengths');
+var volumesRouter = require('./api/volumes');
+var orderRouter = require('./api/order');
 
-// Serve static files from the React app
+var app = express();
+
+var port = normalizePort(process.env.PORT || '9000');
+app.set('port', port);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-    res.json(['a', 'b', 'c']);
+app.use('/', indexRouter);
+app.use('/api/lengths', lengthsRouter);
+app.use('/api/volumes', volumesRouter);
+app.use('/api/order', orderRouter);
+app.use((req, res, next) => {
+    next(createError(404));
+});
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
-});
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-const port = process.env.PORT || 9000;
-app.listen(port);
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
 
-console.log(`App listening on ${port}`);
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+module.exports = app;
