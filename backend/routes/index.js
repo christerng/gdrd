d541ac809f8d45f1a0ab1396fa8382dbb0c14a59
christerng <christerng@gmail.com>
2023-05-20 09:52:49 +0800

    build: this one's for you Raymond

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('Heroku is pulling from GitHub automatically');
+  res.send('Heroku is pulling from GitHub automatically\nYes, this is what you\'re supposed to see');
 });
 
 module.exports = router;
