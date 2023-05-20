9cba2fe4392b0c2130002aa3bdacd8115470e163
christerng <christerng@gmail.com>
2023-05-21 01:21:38 +0800

    style: remove whitespace

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './frontend/build')));
-
 app.use('/api/lengths', lengthsRouter);
 app.use('/api/volumes', volumesRouter);
 app.use('/api/orders', ordersRouter);
