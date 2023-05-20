1e3eecd1b25e94682c1b95d74d9864e8b2549549
Raymond Lu <132875309+toopieare@users.noreply.github.com>
2023-05-20 14:17:26 +0800

    fix: reinstated idTag in Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,10 +2,11 @@ interface Props {
   children: string;
   color?: string;
   onClick: () => void;
+  idTag: string;
 }
-const Button = ({ children, onClick, color = "primary" }: Props) => {
+const Button = ({ children, onClick, color = "primary", idTag }: Props) => {
   return (
-    <button className={"btn btn-" + color} onClick={onClick}>
+    <button className={"btn btn-" + color} onClick={onClick} id={idTag}>
       {children}
     </button>
   );
