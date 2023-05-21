f7cfbbc9ac429931f8511861223343cab43ef8b5
Raymond Lu <132875309+toopieare@users.noreply.github.com>
2023-05-21 11:08:15 +0800

    feat: basic UI for admin view

--- frontend/queue_user/src/App.js
+++ frontend/queue_user/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import menu from "./assets/menu.svg";
 import sportsBar from "./assets/sportsBar.svg";
-import systemstatusBar from "./assets/systemstatusBar.svg";
 import rectangle7 from "./assets/rectangle7.svg";
 import heinekenLogoPng from "./assets/heinekenLogoPng.png";
 import image from "./assets/image.png";
@@ -70,61 +69,55 @@ class App extends Component {
     return (
       <div className="App">
         <div className="bar-location-zone-1-market-strx">
-      <div className="flex-containerx">
-      </div>
-      <div className="nav-barx">
-        <img className="rectangle-7x" src={rectangle7} />
-        <img className="sports-barx" src={sportsBar} />
-        <div className="local-mallx" />
-        <img className="menux" src={menu} />
-        <img className="heineken-logo-pngx" src={heinekenLogoPng} />
-      </div>
-      <div id="rectangle-house" className="rectangle-house">
-        <span className="location-text">Respective Outlet Here</span>
-      </div>
-      <img className="image" src={image} />
-      <div id="rect-1" className="rect-1"></div>
-      <div id="rect-2" className="rect-1"></div>
-      <img id="cupnormal" className="beerPlasticCup" src={beerPlasticCup} />
-      <img id="cup00" className="beerPlasticCup" src={beerPlasticCup} />
-      <img className="imagenormal" src={imagenormal} />
-      <img className="image00" src={image00} />
-      <span className="orig-text">Heineken Original</span>
-      <span className="abv-normal">ABV 5%</span>
-      <span className="price-normal">Cup - $14.00</span>
-      <div>
-        <span className="total-normal" id="total-normal">
-          $0.00
-        </span>
-      </div>
-      <span className="zero-text">Heineken 0.0</span>
-      <span className="abv-00">ABV 0.0%</span>
-      <span className="price-00">Cup - $14.00</span>
-      <div>
-        <span className="total-00" id="total-00">
-          $0.00
-        </span>
-      </div>
-      <form onSubmit={this.handleSubmit}>
-       <label for="orig-count"></label>
-       <input type="button" id="minus-button-1" onClick = {decrementValue} value ="-" />
-       <input type="button" id="plus-button-1" onClick = {incrementValue} value ="+" />
-        <input className="order-count" id="volume" name="volume" type="text" defaultValue ="0" />
-        <label for="queue" id="q-label">Q:</label><br /><br />
-          <input type="text" id="queue" name="queue" defaultValue ="1"></input><br /><br />
-          <label for="volume">Volume:</label><br /><br />
-          <button type="submit" id = "add-to-cart">Submit</button>
-      </form>
-      
-      <form>
-      <label for="zero-count"></label>
-      <input type="button" id="minus-button-2" onClick = {decrementValue0} value ="-" />
-       <input type="button" id="plus-button-2" onClick = {incrementValue0} value ="+" />
-        <input className="order-count" id="zero-count" type="text" value="0" />
-      </form>
-    </div>
-
-      
+          <div className="nav-barx">
+            <img className="rectangle-7x" src={rectangle7} />
+            <img className="sports-barx" src={sportsBar} />
+            <img className="menux" src={menu} />
+            <img className="heineken-logo-pngx" src={heinekenLogoPng} />
+          </div>
+          <div id="rectangle-house" className="rectangle-house">
+            <span className="location-text">Respective Outlet Here</span>
+          </div>
+          <img className="image" src={image} />
+          <div id="rect-1" className="rect-1"></div>
+          <div id="rect-2" className="rect-1"></div>
+          <img id="cupnormal" className="beerPlasticCup" src={beerPlasticCup} />
+          <img id="cup00" className="beerPlasticCup" src={beerPlasticCup} />
+          <img className="imagenormal" src={imagenormal} />
+          <img className="image00" src={image00} />
+          <span className="orig-text">Heineken Original</span>
+          <span className="abv-normal">ABV 5%</span>
+          <span className="price-normal">Cup - $14.00</span>
+          <div>
+            <span className="total-normal" id="total-normal">
+              $0.00
+            </span>
+          </div>
+          <span className="zero-text">Heineken 0.0</span>
+          <span className="abv-00">ABV 0.0%</span>
+          <span className="price-00">Cup - $14.00</span>
+          <div>
+            <span className="total-00" id="total-00">
+              $0.00
+            </span>
+          </div>
+          <form onSubmit={this.handleSubmit}>
+          <label for="orig-count"></label>
+          <input type="button" id="minus-button-1" onClick = {decrementValue} value ="-" />
+          <input type="button" id="plus-button-1" onClick = {incrementValue} value ="+" />
+            <input className="order-count" id="volume" name="volume" type="text" defaultValue ="0" />
+            <label for="queue" id="q-label">Q:</label><br /><br />
+              <input type="text" id="queue" name="queue" defaultValue ="1"></input><br /><br />
+              <label for="volume">Volume:</label><br /><br />
+              <button type="submit" id = "add-to-cart">Submit</button>
+          </form>
+          <form>
+          <label for="zero-count"></label>
+          <input type="button" id="minus-button-2" onClick = {decrementValue0} value ="-" />
+          <input type="button" id="plus-button-2" onClick = {incrementValue0} value ="+" />
+            <input className="order-count" id="zero-count" type="text" value="0" />
+          </form>
+        </div>
       </div>
     );
   };
