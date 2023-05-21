79634e064ed2d7c5b93a782175de4704ed76e4e2
Raymond Lu <132875309+toopieare@users.noreply.github.com>
2023-05-21 14:10:22 +0800

    feat: improved screen to display 4D to queue user

--- frontend/queue_user/src/App.js
+++ frontend/queue_user/src/App.js
@@ -8,6 +8,7 @@ import image from "./assets/image.png";
 import image00 from "./assets/image00.png";
 import imagenormal from "./assets/imagenormal.png";
 import beerPlasticCup from "./assets/beerPlasticCup.png";
+import cheers from "./assets/cheers.svg";
 
 const generate = require('./generate');
 
@@ -48,6 +49,11 @@ const decrementValue0 = () => {
   document.getElementById("total-00").innerHTML = "$" + value * 14 + ".00";
 };
 
+const switchScreen = () => {
+  document.getElementById("screen-1").style.visibility="hidden";
+  document.getElementById("screen-2").style.visibility="visible";
+};
+
 class App extends Component {
   handleSubmit(event) {
     event.preventDefault();
@@ -56,7 +62,14 @@ class App extends Component {
     const volume = event.target.volume.value;
 
     const order = generate();
-    alert(order);
+    document.getElementById("dig-1").innerHTML =
+    order[0];
+    document.getElementById("dig-2").innerHTML =
+    order[1];
+    document.getElementById("dig-3").innerHTML =
+    order[2];
+    document.getElementById("dig-4").innerHTML =
+    order[3];
 
     fetch(
       `/api/orders/queue/${queue}/order/${order}/volume/${volume}`,
@@ -68,7 +81,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="bar-location-zone-1-market-strx">
+        <div className="screen-1" id="screen-1">
           <div className="nav-barx">
             <img className="rectangle-7x" src={rectangle7} />
             <img className="sports-barx" src={sportsBar} />
@@ -109,7 +122,7 @@ class App extends Component {
             <label for="queue" id="q-label">Q:</label><br /><br />
               <input type="text" id="queue" name="queue" defaultValue ="1"></input><br /><br />
               <label for="volume">Volume:</label><br /><br />
-              <button type="submit" id = "add-to-cart">Submit</button>
+              <button type="submit" onClick = {switchScreen} id = "add-to-cart">Submit</button>
           </form>
           <form>
           <label for="zero-count"></label>
@@ -117,6 +130,37 @@ class App extends Component {
           <input type="button" id="plus-button-2" onClick = {incrementValue0} value ="+" />
             <input className="order-count" id="zero-count" type="text" value="0" />
           </form>
+        </div>
+        <div className="screen-2" id="screen-2">
+          <div className="nav-barx">
+            <img className="rectangle-7x" src={rectangle7} />
+            <img className="sports-barx" src={sportsBar} />
+            <div className="local-mallx" />
+            <img className="menux" src={menu} />
+            <img className="heineken-logo-pngx" src={heinekenLogoPng} />
+          </div>
+          <div id="rectangle-bar" className="rectangle-bar"></div>
+          <div id="rectangle-gr-2" className="rectangle-gr-2"></div>
+          <span className="all-set">Cheers to your order! It's all set.</span>
+          <img className="cheers" src={cheers} />
+          <div className="please-display">
+            <span className="please-display-txt">
+              Please display these four digits to the bar counter attendant in order
+              to collect your beer.
+            </span>
+          </div>
+          <div className="giant-num" id="giant-num-1">
+            <span id='dig-1'>0</span>
+          </div>
+          <div className="giant-num" id="giant-num-2">
+            <span id='dig-2'>0</span>
+          </div>
+          <div className="giant-num" id="giant-num-3">
+            <span id='dig-3'>0</span>
+          </div>
+          <div className="giant-num" id="giant-num-4">
+            <span id='dig-4'>0</span>
+          </div>
         </div>
       </div>
     );
