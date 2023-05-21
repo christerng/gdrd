dcd48eee7b6267da6dbc288f073c5385c2ad0993
Raymond Lu <132875309+toopieare@users.noreply.github.com>
2023-05-21 11:08:38 +0800

    feat: basic UI for admin_user

--- frontend/admin_user/src/App.js
+++ frontend/admin_user/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import menu from "./assets/menu.svg";
+import sportsBar from "./assets/sportsBar.svg";
+import rectangle7 from "./assets/rectangle7.svg";
+import heinekenLogoPng from "./assets/heinekenLogoPng.png";
 
 class App extends Component {
   state = { lengths: {}, volumes: {} };
@@ -16,39 +20,71 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Admin Interface</h1>
-        <table border='1'>
-          <tr>
-            <th>Queue</th>
-            <th>Length</th>
-            <th>Volume</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>{this.state.lengths[1]}</td>
-            <td>{this.state.volumes[1]}</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>{this.state.lengths[2]}</td>
-            <td>{this.state.volumes[2]}</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>{this.state.lengths[3]}</td>
-            <td>{this.state.volumes[3]}</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>{this.state.lengths[4]}</td>
-            <td>{this.state.volumes[4]}</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>{this.state.lengths[5]}</td>
-            <td>{this.state.volumes[5]}</td>
-          </tr>
-        </table>
+        <div className="bar-location-zone-1-market-strx">
+          <div className="nav-barx">
+            <img className="rectangle-7x" src={rectangle7} />
+            <img className="sports-barx" src={sportsBar} />
+            <img className="menux" src={menu} />
+            <img className="heineken-logo-pngx" src={heinekenLogoPng} />
+          </div>
+          <div id="rectangle-house" className="rectangle-house">
+            <span className="location-text">Admin View</span>
+          </div>
+          <div id="header-1" className="header">
+            <span className="header-text">Queue</span>
+          </div>
+          <div id="header-2" className="header">
+            <span className="header-text">Length</span>
+          </div>
+          <div id="header-3" className="header">
+            <span className="header-text">Volume</span>
+          </div>
+          <div id="queue-1" className="queue">
+            <span className="queue-text">1</span>
+          </div>
+          <div id="queue-2" className="queue">
+            <span className="queue-text">2</span>
+          </div>
+          <div id="queue-3" className="queue">
+            <span className="queue-text">3</span>
+          </div>
+          <div id="queue-4" className="queue">
+            <span className="queue-text">4</span>
+          </div>
+          <div id="queue-5" className="queue">
+            <span className="queue-text">5</span>
+          </div>
+          <div id="length-1" className="length">
+            <span className="length-text">{this.state.lengths[1]}</span>
+          </div>
+          <div id="length-2" className="length">
+            <span className="length-text">{this.state.lengths[2]}</span>
+          </div>
+          <div id="length-3" className="length">
+            <span className="length-text">{this.state.lengths[3]}</span>
+          </div>
+          <div id="length-4" className="length">
+            <span className="length-text">{this.state.lengths[4]}</span>
+          </div>
+          <div id="length-5" className="length">
+            <span className="length-text">{this.state.lengths[5]}</span>
+          </div>
+          <div id="volume-1" className="volume">
+            <span className="volume-text">{this.state.volumes[1]}</span>
+          </div>
+          <div id="volume-2" className="volume">
+            <span className="volume-text">{this.state.volumes[2]}</span>
+          </div>
+          <div id="volume-3" className="volume">
+            <span className="volume-text">{this.state.volumes[3]}</span>
+          </div>
+          <div id="volume-4" className="volume">
+            <span className="volume-text">{this.state.volumes[4]}</span>
+          </div>
+          <div id="volume-5" className="volume">
+            <span className="volume-text">{this.state.volumes[5]}</span>
+          </div>
+      </div>
       </div>
     );
   };
